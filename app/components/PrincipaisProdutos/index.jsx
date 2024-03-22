b45ffb1147ea5fb3713cb1e8603e1302465f9942
Milena Julião <milenadjuliao@gmail.com>
2024-03-22 07:41:23 -0300

    Teste background.

--- app/components/PrincipaisProdutos/index.jsx
+++ app/components/PrincipaisProdutos/index.jsx
@@ -1,9 +1,12 @@
+import CarrosselProdutos from '../elements/CarrosselProdutos'
 import styles from './styles.module.scss'
 
 export default function PrincipaisProdutos(){
     return(
         <div className={styles.container}>
             <h1>Gostaria de entrar e tomar uma xícara de café?</h1>
+
+            <CarrosselProdutos/>
         </div>
     )
 }
