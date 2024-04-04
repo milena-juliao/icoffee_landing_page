fc5ff6367623e89fc7f730d2575c6fa02d57f82d
Milena Julião <milenadjuliao@gmail.com>
2024-04-04 07:38:38 -0300

    Backgrounds e imagens

--- app/page.js
+++ app/page.js
@@ -4,8 +4,9 @@ import Hero from "./components/Hero";
 import PrincipaisProdutos from "./components/PrincipaisProdutos";
 import Footer from "./components/Footer";
 import Nav from "./components/Nav";
-import OrigemGrao from "./components/Etapas";
+import OrigemGrao from "./components/OrigemGrao";
 import EmbalagemEspecial from "./components/EmbalagemEspecial";
+import Etapas from "./components/Etapas";
 
 export default function Home() {
     return (
@@ -13,8 +14,9 @@ export default function Home() {
             <Nav/>
             <Hero/>
             <PrincipaisProdutos/>
-            <OrigemGrao/>
+            <OrigemGrao />
             <EmbalagemEspecial/>
+            <Etapas/>
             <Footer/>
         </div>
     );
