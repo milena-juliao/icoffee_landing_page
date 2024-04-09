08465b5cd3ab178ed6868e89035a29cab47aa702
Milena Julião <milenadjuliao@gmail.com>
2024-04-09 07:27:20 -0300

    Ajustes produto.

--- app/components/elements/CarrosselProdutos/index.jsx
+++ app/components/elements/CarrosselProdutos/index.jsx
@@ -4,12 +4,12 @@ export default function CarrosselProdutos() {
     return (
         <div className={styles.container}>
             <div className={styles.content}>
-                <img src="" alt="Imagem do produto x"/>
+                <img src="/images/produtos/produto_1.png" alt="Imagem do produto x"/>
                 <h3>Café Tipo 1</h3>
-                <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.</p>
-                <p>R$ 999,99</p>
+                <p className={styles.desc}>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.</p>
+                <p className={styles.preco}>R$ 999,99</p>
                 <div className={styles.btnsCart}>
-                    <button>Cart</button>
+                    <button><img src="images/icons/cart_product_icon.png" /></button>
                     <button className={styles.btnComprar}>Comprar</button>
                 </div>
             </div>
